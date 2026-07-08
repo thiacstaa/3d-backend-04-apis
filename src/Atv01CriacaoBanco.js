43b879cbdd8e11093251bae4854fa133e5c7da15
thiacstaa <anthoniely.costa@nave.org.br>
2026-07-08 09:54:46 -0300

    Update Atv01CriacaoBanco.js

--- src/Atv01CriacaoBanco.js
+++ src/Atv01CriacaoBanco.js
@@ -24,3 +24,60 @@
   * Ao final deste arquivo, use "module.exports = app" para
   * exportar o objeto do servidor para os testes automatizados.
   */
+const express = require('express')
+const path = require('path')
+const cors = require('cors');
+// Importando a ferramenta pra utilizar bancos de dados
+const sql = require('sqlite3').verbose()
+
+// Definindo a porta usada para abrir o servidor (no final deste código)
+const porta = 3000
+
+const app = express()
+// Necessário para leitura do corpo de uma requisição
+app.use(express.urlencoded({ extended: true }))
+// Necessário para interpretação do formato JSON
+app.use(express.json())
+// Para evitar erros de CORS
+app.use(cors());
+// Para configurar o servidor para olhar dentro da pasta src por padrão
+app.use(express.static(path.join(__dirname, 'src')));
+
+const db = new sql.Database(
+  './beyblade.db', // Nome do arquivo do banco de dados
+  (erro) => { // Função que executa que o banco é criado
+    if (erro) {
+      console.error('Erro ao abrir o banco de dados "beyblade.db":', erro.message);
+    } else {
+      console.log('Conectado ao banco de dados SQLite3 "beyblade.db"');
+    }
+  }
+)
+db.run(
+  // 1º argumento = comando SQL
+  `CREATE TABLE IF NOT EXISTS beyblades (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome TEXT NOT NULL UNIQUE,
+  lamina TEXT,
+  catraca TEXT,
+  ponta TEXT,
+  participante NOT NULL UNIQUE
+  )`,
+  // 2º argumento = função executada após termos o resultado do comando SQL
+  (erro) => {
+    if (erro) {
+      console.error('Erro ao criar a tabela "beyblades"', erro.message);
+    } else {
+      console.log('Tabela "beyblades" pronta!');
+    }
+  }
+)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtv.html'))
+})
+
+app.listen(porta, () => {
+  console.log(`Servidor rodando em http://localhost:${porta}`)
+})
+
+module.exports = app
